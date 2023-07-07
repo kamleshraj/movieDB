664a290c7c6a47d7c88db6b8cb7997bb9e2e7e6d
kamleshraj <kamleshk.uiuxdesigner@gmail.com>
2023-07-07 17:47:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from 'react';
 
-function App() {
+import { fetchDataFromApi } from './utils/api';
+
+import{useSelector,useDispatch} from 'react-redux'
+import { getApiConfiguration, getGenres } from './store/homeSlice';
+import Home from './pages/home/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Details from './pages/details/Details';
+import Explore from './pages/explore/Explore';
+import PageNotFound from './pages/404/PageNotFound';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import SearchResults from './pages/searchResults/SearchResults';
+
+const App =()=>{
+  const dispatch = useDispatch();
+  const {url} = useSelector((state)=>state.home)
+
+
+  useEffect(()=>{
+    fetchApiConfig()
+    genresCall()
+  },[])
+
+  const genresCall= async()=>{
+    let promises = [];
+    let endPoints=["tv","movie"];
+    let allGenres={}
+
+    endPoints.forEach((url)=>{
+      promises.push(fetchDataFromApi(`/genre/${url}/list`))
+    })
+    const data = await Promise.all(promises);
+    data.map(({genres})=>{
+      return genres.map((item)=>(allGenres[item.id]=item))
+    })
+    dispatch(getGenres(allGenres))
+  }
+  const fetchApiConfig = ()=>{
+    fetchDataFromApi('/configuration')
+    .then((res)=>{
+      const url ={
+        backdrop:res.images.secure_base_url + 'original',
+        poster:res.images.secure_base_url + 'original',
+        profile:res.images.secure_base_url + 'original'
+      }
+      dispatch(getApiConfiguration(url))
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/:mediaType/:id' element={<Details/>}/>
+
+        <Route path='/explore/:mediaType' element={<Explore/>}/>
+
+        <Route path='/search/:query' element={<SearchResults/>}/>
+
+        <Route path='*' element={<PageNotFound/>}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
