81d262134923ed1be835baa73a5beacb69f587f7
kamleshraj <kamleshk.uiuxdesigner@gmail.com>
2023-07-07 18:13:04 +0530

    build data

--- src/App.js
+++ src/App.js
@@ -55,11 +55,8 @@ const App =()=>{
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/:mediaType/:id' element={<Details/>}/>
-
         <Route path='/explore/:mediaType' element={<Explore/>}/>
-
         <Route path='/search/:query' element={<SearchResults/>}/>
-
         <Route path='*' element={<PageNotFound/>}/>
       </Routes>
       <Footer/>
